ae31877e778fc30bbd98673d75f457ce61fdffd1
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-03-08 18:16:29 +0100

    Add project files.

---
+++ OperatoryLogiczne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatoryLogiczne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operatory logiczne
+            //  >  - większość
+            //  <  - mniejszość
+            //  ==  - równość
+            //  !=  - nie równość
+            //  >=  - większy bądź równy
+            //  <=  - mniejszy bądź równy
+
+            //trudniejsze operatory
+            // &&  -  i - wszystkie warunki muszą zostać spełnione    prawda && prawda = prawda     prawda && fałsz = fałsz
+            // ||  -  lub - którykolwiek z warunków musi zostać spełniony      prawda && fałsz = prawda   fałsz && fałsz = fałsz
+            //  !  -  przeciwieństwo   !prawda = fałsz     !fałsz = prawda;
+
+            bool wynik = 70 == 70 || 21 != 21 || false;
+            Console.WriteLine(wynik);
+
+            // inkrementacja i dekrementacja
+
+            int x = 1;
+
+            //x = x + 1;    =>    x++;
+            //x = x - 1;    =>    x--;
+
+            x++;
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Semestr1_Lekcja5_2023/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semestr1_Lekcja5_2023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal kosztFilamentu = decimal.Parse(textBox1.Text);
+            int zuzytyFilamet = int.Parse(textBox2.Text);
+
+            //jak obliczyć koszt materiału?
+            //koszt filamentu zł za 1kg = 1000g
+            //zużyty filament w gramach
+
+            decimal kosztMateriału = (kosztFilamentu * zuzytyFilamet) / 1000;
+
+            label3.Text = "Cena materiału wynosi: " + Math.Round(kosztMateriału,2) +"zł";
+
+            // (mocDrukarki * czasWydruku) / 1000  * stawka - obliczanie ceny prądu
+
+            const decimal stawka = 0.95M;
+
+            int mocDrukarki = int.Parse(textBox3.Text);
+            int czasWydruku = int.Parse(textBox4.Text);
+
+            decimal cenaPradu = (mocDrukarki * czasWydruku) / 1000M * stawka;
+
+            label6.Text = "Cena prądu wynosi: " + cenaPradu +"zł";
+            label7.Text = "Razem: " + (kosztMateriału + cenaPradu+"zł");
+
+        }
+    }
+}
